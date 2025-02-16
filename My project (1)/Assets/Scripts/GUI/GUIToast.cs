0c0f7b42ad434225b160dfd74fe264c89828063d
cruise4u <pedrofrancisco90@gmail.com>
2025-02-16 16:49:16 +0100

    GUI testing

---
+++ My project (1)/Assets/Scripts/GUI/GUIToast.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class GUIToast : MonoBehaviour
+{
+    EGUICombatToastType GUICombatToastType;
+    private GUIToastAnimation _animation;
+
+    public void WriteValue(TextMeshProUGUI textBox,string text)
+    {
+        textBox.text = text;
+    }
+
+    public void ShowMessage(TextMeshProUGUI textBox)
+    {
+        textBox.enabled = true;
+    }
+
+    public void HideMessage(TextMeshProUGUI textBox)
+    {
+        textBox.enabled = false;
+    }
+}
